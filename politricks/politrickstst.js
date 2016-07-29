2b1442d88642e638b5a64423a03a17f9f61576f4
adriannab99 <adriannaburton99@gmail.com>
2016-07-29 13:25:33 -0700

    Independent() update

--- politricks/politrickstst.js
+++ politricks/politrickstst.js
@@ -30,3 +30,26 @@ function Republican(){
 		var x = sessionStorage.setItem('x', var1); 
 		document.getElementById("relativeSlider").style.left = x; 
 	}
+
+function Independent(){
+	var elem = document.getElementById("relativeSlider");
+		var var1 = window.getComputedStyle(elem, null).getPropertyValue("left");
+		var1 = var1.replace("px", "");
+		console.log(var1);
+		var1 = parseInt(var1);
+		console.log(var1);
+		if (var1 < 0)
+		{
+			var1 += 20;
+		}
+		else if (var1 > 0)
+		{
+			var1 -= 20;
+		}
+		console.log(var1);
+		var1 = String(var1);
+		console.log(var1);
+		var1 = var1 + "px"; 
+		var x = sessionStorage.setItem('x', var1); 
+		document.getElementById("relativeSlider").style.left = x; 
+}
