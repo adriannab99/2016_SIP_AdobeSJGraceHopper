ecce5815698ccc2a0f53a310f2d33d94f49886a8
alia-ahmed22 <aliaahmed424@gmail.com>
2016-07-29 09:52:14 -0700

    Updates and Hopeful Functionality

--- politricks/politrickstst.js
+++ politricks/politrickstst.js
@@ -1,31 +1,32 @@
-var elem = document.getElementById("relativeSlider");
-var var1 = window.getComputedStyle(elem, null).getPropertyValue("left");
-
 
 function Democrat(){
+		var elem = document.getElementById("relativeSlider");
+		var var1 = window.getComputedStyle(elem, null).getPropertyValue("left");
 		var1 = var1.replace("px", "");
 		console.log(var1);
 		var1 = parseInt(var1);
 		console.log(var1);
-		var1 -= 50;
+		var1 -= 20;
 		console.log(var1);
 		var1 = String(var1);
 		console.log(var1);
 		var1 = var1 + "px"; 
-		var x = localStorage.setItem('x', var1); 
+		var x = sessionStorage.setItem('x', var1); 
 		document.getElementById("relativeSlider").style.left = x; 
 	}
 
 function Republican(){
+		var elem = document.getElementById("relativeSlider");
+		var var1 = window.getComputedStyle(elem, null).getPropertyValue("left");
 		var1 = var1.replace("px", "");
 		console.log(var1);
 		var1 = parseInt(var1);
 		console.log(var1);
-		var1 += 50;
+		var1 += 20;
 		console.log(var1);
 		var1 = String(var1);
 		console.log(var1);
 		var1 = var1 + "px"; 
-		var x = localStorage.setItem('x', var1); 
+		var x = sessionStorage.setItem('x', var1); 
 		document.getElementById("relativeSlider").style.left = x; 
 	}
